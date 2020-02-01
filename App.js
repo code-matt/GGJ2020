3fa418d8a11352d15d2d9f21f4de6bed921ece19
Matthew Thompson <code.matthew.thompson@gmail.com>
2020-01-31 20:08:40 -0500

    started scaffolding main menu

--- App.js
+++ App.js
@@ -6,15 +6,49 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {View} from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, Dimensions, SafeAreaView} from 'react-native';
 
 import {ARKit} from 'react-native-arkit';
 
 class App extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      gameStarted: false,
+      isHost: false,
+      connectedPeers: [],
+      starshipState: {
+        isRepaired: false,
+        parts: {
+          engine: {
+            isRepaired: false,
+            enginePosition: {x: 0, y: 0, z: 0}, // this is the starting world position of the engine that needs to get to the
+            // ship in order to repair it.
+          },
+        },
+      },
+    };
+  }
 
-  handlePress = async (e) => {
+  renderMainMenu = () => {
+    return (
+      <SafeAreaView
+        style={{
+          position: 'absolute',
+          flex: 1,
+          width: Dimensions.get('window').width,
+          height: Dimensions.get('window').height,
+          zIndex: 2,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Text style={{ color: '#fff', fontSize: 32 }}>REPAIR IT</Text>
+      </SafeAreaView>
+    );
+  };
+
+  handlePress = async e => {
     let hits = await ARKit.hitTestPlanes(
       {
         x: e.nativeEvent.pageX,
@@ -23,25 +57,28 @@ class App extends Component {
       1,
     );
 
-    console.log(hits)
-  }
+    console.log(hits);
+  };
   render() {
     return (
-      <View
-        style={{flex: 1, position: 'relative'}}
-        onTouchEnd={e => {
-          console.log('touched');
-          this.touchXStart = e.nativeEvent.pageX;
-          this.handlePress(e);
-        }}>
-        <ARKit
-          style={{flex: 1}}
-          debug
-          planeDetection={ARKit.ARPlaneDetection.Horizontal}
-          lightEstimationEnabled
-          onARKitError={console.log} // if arkit could not be initialized (e.g. missing permissions), you will get notified here
-        />
-      </View>
+      <>
+        {!this.state.gameStarted && this.renderMainMenu()}
+        <View
+          style={{flex: 1, position: 'relative'}}
+          onTouchEnd={e => {
+            console.log('touched');
+            this.touchXStart = e.nativeEvent.pageX;
+            this.handlePress(e);
+          }}>
+          <ARKit
+            style={{flex: 1}}
+            debug
+            planeDetection={ARKit.ARPlaneDetection.Horizontal}
+            lightEstimationEnabled
+            onARKitError={console.log} // if arkit could not be initialized (e.g. missing permissions), you will get notified here
+          />
+        </View>
+      </>
     );
   }
 }
